5acb5278882e6775a48e6759c6e0b97599f7bcff
Steve Hopkins <sh_ufton@hotmail.com>
2016-10-19 10:41:50 +0100

    Detect palindrome from string.

---
+++ Palindromes/PalindromeFinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palindromes
+{
+    internal class PalindromeFinder
+    {
+        internal bool IsPalindrome(string inputString)
+        {
+            if(String.IsNullOrEmpty(inputString))
+                throw new ArgumentException("A valid string must be passed to IsPalindrome.",nameof(inputString));
+
+            int fwPos = 0, revPos = inputString.Length - 1;
+
+            while (fwPos < revPos)
+            {
+                if (inputString[fwPos] != inputString[revPos])
+                    return false;
+                ++fwPos;
+                --revPos;
+            }
+            return true;
+        }
+    }
+}
---
+++ PalindromesTests/PalindromeFinderTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Palindromes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromesTests
+{
+    [TestClass]
+    public class PalindromeFinderTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A null string was not detected.")]
+        public void IsPalindrome_NullString_ThrowsArgumentException()
+        {
+            var pf = new PalindromeFinder();
+
+            pf.IsPalindrome(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "An empty string was not detected.")]
+        public void IsPalindrome_EmptyString_ThrowsArgumentException()
+        {
+            var pf = new PalindromeFinder();
+
+            pf.IsPalindrome(String.Empty);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_NonPalindrome_ReturnsFalse()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("AOIFSJGOADIFJGOADIFJ");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_CloseButNonPalindrome_ReturnsFalse()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("ASDFGHJKLLJHGFDSA");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_SingleCharacterString_ReturnsTrue()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("S");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_ActualPalindromeOfSingleCharacter_ReturnsTrue()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("SSSSSSSSSSSSSSSSSSS");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_ActualPalindromeWithEvenLength_ReturnsTrue()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("STTS");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPalindrome_ActualPalindromeWithOddLength_ReturnsTrue()
+        {
+            var pf = new PalindromeFinder();
+
+            var result = pf.IsPalindrome("STUTS");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
